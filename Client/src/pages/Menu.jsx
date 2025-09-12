1528497f94d295b08185817e29be355983bf49c0
Svetozar Slivarov <slivarov.Svetlio2907@gmail.com>
2025-09-12 12:34:29 +0300

    Normalize: keep only 'client' (lowercase); remove 'Client'

--- Client/src/pages/Menu.jsx
+++
@@ -1,8 +0,0 @@
-export default function Menu() {
-    return (
-        <div className="page">
-            <h2>Menu</h2>
-            <p>Explore our pizzas and deals.</p>
-        </div>
-    );
-}
